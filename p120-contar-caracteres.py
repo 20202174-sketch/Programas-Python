bfa68012517e99542049fbacfd2efb2ede81f19b
20202174-sketch <20202174@uaz.edu.mx>
2025-11-24 00:03:46 -0600

    Tarea 6

--- p120-contar-caracteres.py
+++ p120-contar-caracteres.py
@@ -1,5 +1,11 @@
 # p120-contar-caracteres.py
-# Contar cuántas veces aparece cada carácter en una cadena
+# Escribe un programa que pida al usuario que ingrese una cadena de texto.
+# • Crea un diccionario vacío para almacenar la frecuencia (cantidad de apariciones) de cada carácter.
+# • Itera sobre cada carácter en la cadena ingresada.
+# • En cada iteración:
+# o Si el carácter no existe como llave en el diccionario, agrégalo con un valor de 1.
+# o Si el carácter ya existe, incrementa su valor actual en 1.
+# • Al finalizar el ciclo, muestra el diccionario resultante con el conteo de caracteres.
 
 print('\033[H\033[J')
 print("Frecuencia de caracteres en un texto\n")
